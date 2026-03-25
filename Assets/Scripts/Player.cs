1473546ede6bb622772c8206190bd057f3457e31
pferna7 <pferna7@uwo.ca>
2026-03-25 13:57:12 -0400

    added left and right movement with arrow keys/A&D
    do not update any files when opening

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+}
