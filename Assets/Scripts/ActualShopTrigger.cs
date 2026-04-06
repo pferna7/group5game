c685a94f4c0fbc8786028a7bb131602031593b56
pferna7 <pferna7@uwo.ca>
2026-04-06 12:14:23 -0400

    shop opens at end of hassaan's level

---
+++ Assets/Scripts/ActualShopTrigger.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ActualShopTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.AddCoins(100);  // cleaner method
+            }
+
+            StartCoroutine(LoadShop());
+        }
+    }
+
+    private IEnumerator LoadShop()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    private void OnGUI()
+    {
+        if (triggered)
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 40;
+            style.fontStyle = FontStyle.Bold;
+            style.normal.textColor = Color.yellow;
+            style.alignment = TextAnchor.MiddleCenter;
+            GUI.Label(new Rect(0, Screen.height / 2 - 30, Screen.width, 60),
+                "You received 100 coins!", style);
+        }
+    }
+}
