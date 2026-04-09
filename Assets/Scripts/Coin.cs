32e2a271d72acaf6db4c23b664b06e9c3a32971d
Naufil06 <124323343+Naufil06@users.noreply.github.com>
2026-04-08 23:12:41 -0400

    added naufil final cmmit

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
 
+        NaufilPlayer player = collision.GetComponent<NaufilPlayer>();
 
-private void OnTriggerEnter2D(Collider2D collision) {
+        if (player == null)
+        {
+            player = collision.GetComponentInParent<NaufilPlayer>();
+        }
 
-    if (collision.gameObject.tag == "Player") {
-
-        Player player = collision.gameObject.GetComponent<Player>();
-        player.coins += 1;
-        Destroy(gameObject);
-        Debug.Log("Added coin total: " + player.coins);
+        if (player != null)
+        {
+            player.AddCoins(1);
+            Destroy(gameObject);
+            Debug.Log("Added coin total: " + player.coins);
+        }
     }
-
-}
-
 }
